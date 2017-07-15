d2aeed27fd710ac80bad0bb94792fa830f02b426
Pencil <9807175@qq.com>
2017-07-15 21:43:25 +0800

    First commit

---
+++ Learning Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Swift
+//
+//  Created by 陈林 on 15/07/2017.
+//  Copyright © 2017 Pencil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
