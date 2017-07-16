e001f88bc3adcd2999b1d32bf0d2e214c3a5d6d3
Pencil <9807175@qq.com>
2017-07-16 17:04:08 +0800

    1.1 Calculator

--- Learning Swift/ViewController.swift
+++ Learning Swift/ViewController.swift
@@ -2,24 +2,73 @@
 //  ViewController.swift
 //  Learning Swift
 //
-//  Created by 陈林 on 15/07/2017.
+//  Created by Chen lin on 15/07/2017.
 //  Copyright © 2017 Pencil. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isTyping: Bool = false
+    var operation: String?
+    var lastOperand:  Double = 0
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        var current: String
+        if isTyping {
+            current = display.text! + digit
+        } else {
+            current = digit
+            isTyping = true
+        }
+        
+        display.text = current
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let methodSymbol = sender.currentTitle {
+            let displayValue = Double(display.text!)!
+            
+            switch methodSymbol {
+            case "π":
+                lastOperand = Double.pi
+            case "√":
+                lastOperand = sqrt(displayValue)
+            case "cos":
+                lastOperand = cos(displayValue)
+            case "±":
+                lastOperand = -displayValue
+            case "=":
+                lastOperand = calculate(with: displayValue)
+            default:
+                operation = methodSymbol
+            }
+            isTyping = false
+            display.text = String(lastOperand)
+        }
     }
-
+    
+    private func calculate(with secondOperand: Double) -> Double {
+        if let op = operation {
+            operation = nil
+            switch op {
+            case "+":
+                return lastOperand + secondOperand
+            case "-":
+                return lastOperand - secondOperand
+            case "×":
+                return lastOperand * secondOperand
+            case "÷":
+                return lastOperand / secondOperand
+            default:
+                break
+            }
+        }
+        return lastOperand
+    }
 
 }
 
